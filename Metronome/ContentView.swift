cadefdbb703ed8f39215d7a73c802534a596a731
ken <jitamm20081@gmail.com>
2025-08-26 09:00:55 +0800

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by ken on 2025/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by ken on 2025/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
